a04676a05fe56658f300d03b244080fc23b59bf5
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2022-07-25 17:52:39 -0300

    Add placeholder images for carousels

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ function contentLoaded() {
   const backstageCarousel = document.getElementById('backstage-carousel');
 
   function carouselFunctionality(carousel) {
+
+    // Seleccionar los elementos dentro del carousel que se reciba como argumento
     const track = carousel.querySelector('.carousel__track');
     const slides = Array.from(track.children);
 
-    let nextButton = carousel.querySelector('.carousel__button--right');
-    let prevButton = carousel.querySelector('.carousel__button--left');
+    const nextButton = carousel.querySelector('.carousel__button--right');
+    const prevButton = carousel.querySelector('.carousel__button--left');
 
-    let slideWidth = slides[0].getBoundingClientRect().width;
+    const slideWidth = slides[0].getBoundingClientRect().width;
 
     // Posicionar cada slide al lado de la otra
     const setSlidePosition = (slide, index) => {
@@ -27,34 +29,30 @@ function contentLoaded() {
       targetSlide.classList.add('current-slide');
     }
 
+    // Agregar event listeners para los botones de avance y retroceso
     prevButton.addEventListener('click', e => {
       const currentSlide = track.querySelector('.current-slide');
       const prevSlide = currentSlide.previousElementSibling;
 
+        // Solo ir a la slide siguiente si esta existe
       if(currentSlide.previousElementSibling) {
         moveToSlide(track, currentSlide, prevSlide);
       }
-
-    })
+    });
 
     nextButton.addEventListener('click', e => {
       const currentSlide = track.querySelector('.current-slide');
       const nextSlide = currentSlide.nextElementSibling;
 
+      // Solo ir a la slide siguiente si esta existe
       if(currentSlide.nextElementSibling) {
         moveToSlide(track, currentSlide, nextSlide);
       }
-
-    })
+    });
   }
 
+  // Llamar a la función para cada carousel de la página
   carouselFunctionality(galleryCarousel);
   carouselFunctionality(backstageCarousel);
-
-
-
-
-
-
 
 }
