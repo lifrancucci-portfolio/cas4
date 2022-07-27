a64d0dd4079e4a7343f70468dc6e69de64b27c66
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2022-07-27 11:24:14 -0300

    Finish basic layout for individual pages; add nav menu

--- js/main.js
+++ js/main.js
@@ -15,6 +15,26 @@ function contentLoaded() {
     }
   } (0));
 
+  // MENU
+  const navLogo = document.getElementById('nav-title');
+  const navMenu = document.getElementById('nav-ul');
+
+  function toggleMenu() {
+    if(!navMenu.classList.contains('show-menu')) {
+      navMenu.classList.add('show-menu');
+    } else {
+      navMenu.classList.remove('show-menu');
+    }
+  }
+  function hideOnScroll() {
+    if(navMenu.classList.contains('show-menu')) {
+      navMenu.classList.remove('show-menu');
+    }
+  }
+
+  navLogo.addEventListener('click', toggleMenu);
+  window.addEventListener('scroll', hideOnScroll);
+
 
   // CAROUSELS
   const galleryCarousel = document.getElementById('gallery-carousel');
