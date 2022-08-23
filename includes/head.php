650b662ae09e2d9d1a95368278b1083fe84cb38d
Lisandro Francucci <lisandro.francucci@gmail.com>
2022-08-23 19:37:56 -0300

    Minor cleanup

--- includes/head.php
+++ includes/head.php
@@ -39,5 +39,14 @@
     });
   </script>
 
-
 </head>
+
+ <!-- Google tag (gtag.js) -->
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-HWYS6HQ28Z"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HWYS6HQ28Z');
+  </script>
